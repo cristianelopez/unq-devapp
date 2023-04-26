9586c5bce2ceb4a3f67a4156c30385f478a53100
Cristian López <cristianelopez@gmail.com>
2023-04-26 18:08:15 -0300

    delete projects

--- src/spring-hibernate/src/main/java/ar/edu/unq/services/PersonService.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unq.services;
-
-import ar.edu.unq.model.Person;
-
-public class PersonService extends GenericService<Person> {
-
-	private static final long serialVersionUID = 2131359482422367092L;
-
-}
